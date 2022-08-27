f8f7609b9e04e958d8bdd5cc04acff220117b5d8
LIZZETH GOMEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2022-08-27 09:47:51 -0500

    Update

---
+++ codigo_1.R
@@ -0,0 +1,18 @@
+     
+  # ####################################################
+  #             Taller de GitHub con R
+  # ####################################################
+  
+  # Introduccion a GitHub
+  # 1. Instalar Git
+  # 2. Crear repositorio desde GitHub
+  # 3. Crear un proyecto con version control (Git)
+  # 4. Clonar repositorio a la computadora
+  
+  # Crear codigo
+  df <- mtcars
+  names(mtcars)
+  
+  summary(mtcars)
+  plot(df$hp, main = "Grafico")
+  
